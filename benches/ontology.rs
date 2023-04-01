6880bd2b355713b2ca2abcf4e5e91849fda258d4
Jonas Marcello <jonas.marcello@esbme.com>
2023-04-01 16:02:22 +0200

    Add Criterion benchmarks for ontology building and ancestor finding

---
+++ benches/ontology.rs
@@ -0,0 +1,21 @@
+use hpo::Ontology;
+use std::{fs::File, time::Duration};
+use std::io::Read;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn build_ontology_benchmark(c: &mut Criterion) {
+    let mut bytes = Vec::new();
+    File::open("tests/ontology.hpo").expect("ontology.hpo cannot be opened").read_to_end(&mut bytes).expect("cannot read ontology.hpo");
+    c.bench_function("build ontology", |b| {
+        b.iter(|| Ontology::from_bytes(black_box(&bytes[..])).expect("requires valid bytes").len())
+    });
+}
+
+
+criterion_group!{
+    name = ontology;
+    config = Criterion::default().sample_size(20).measurement_time(Duration::from_secs(10));
+    targets = build_ontology_benchmark
+}
+criterion_main!(ontology);
--- benches/similarity.rs
+++ benches/similarity.rs
@@ -6,10 +6,10 @@ use hpo::term::InformationContentKind;
 use hpo::similarity::Builtins;
 use hpo::Ontology;
 
-fn bench(ontology: &Ontology, times: usize) -> usize {
+fn graphic_sequential(ontology: &Ontology, times: usize) -> usize {
     let ic = Builtins::GraphIc(InformationContentKind::Omim);
     let mut count = 0usize;
-    for term1 in ontology.into_iter().skip(1000).take(times) {
+    for term1 in ontology.into_iter().skip(300).take(times) {
         for term2 in ontology.into_iter().skip(500).take(times) {
             let overlap = term1.similarity_score(&term2, &ic);
             if overlap > 0.7 {
@@ -20,12 +20,12 @@ fn bench(ontology: &Ontology, times: usize) -> usize {
     count
 }
 
-fn parallel(ontology: &Ontology, times: usize) -> usize {
+fn graphic_parallel(ontology: &Ontology, times: usize) -> usize {
     let ic = Builtins::GraphIc(InformationContentKind::Omim);
 
     ontology
         .into_iter()
-        .skip(1000)
+        .skip(800)
         .take(times)
         .par_bridge()
         .map(|term1| {
@@ -42,16 +42,17 @@ fn parallel(ontology: &Ontology, times: usize) -> usize {
 }
 
 fn graphic_benchmark(c: &mut Criterion) {
-    let ontology = Ontology::from_standard("./example_data/").unwrap();
+    let ontology = Ontology::from_binary("tests/ontology.hpo").unwrap();
 
-    c.bench_function("graphic 100", |b| {
-        b.iter(|| bench(black_box(&ontology), black_box(100)))
+    c.bench_function("graphic 300", |b| {
+        b.iter(|| graphic_sequential(black_box(&ontology), black_box(300)))
     });
 
     c.bench_function("graphic-parallel 1000", |b| {
-        b.iter(|| parallel(black_box(&ontology), black_box(1000)))
+        b.iter(|| graphic_parallel(black_box(&ontology), black_box(1000)))
     });
 }
+
 
 criterion_group!(similarity, graphic_benchmark);
 criterion_main!(similarity);
